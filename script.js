7c08347d3283cf90f400daa170eb92c24f26fc26
marco <marcoadrianzambra@gmail.com>
2025-05-06 10:04:15 -0500

    feature(page-srtructure) html, script and css done

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const cancelBtn = document.querySelector('.btn-cancel');
+    const sendBtn = document.querySelector('.btn-submit');
+    const inputs = form.querySelectorAll('input');
+
+    function cleanInputs() {
+        inputs.forEach(input => {
+            input.value = '';
+        })
+    }
+
+    // Evento para el botón Cancelar
+    cancelBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (confirm('¿Estás seguro de que deseas cancelar? Se perderán todos los datos ingresados.')) {
+            cleanInputs()
+        }
+    });
+
+    // Evento para el botón Enviar
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let idAllCorrect = true;
+        inputs.forEach(input => {
+            idAllCorrect = !input.value.trim() ? false : true;
+        })
+        if (idAllCorrect) {
+            alert('Datos enviados correctamente');
+            cleanInputs();
+        }
+    })
+})
